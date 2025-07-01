d42787cfc2e3f356b5f2cac65d5e0664e063b579
albertoaraujo <alberto.araujoo@gmail.com>
2025-07-01 12:58:21 -0300

    feat: new metadata titles

--- src/app/ofertas/page.tsx
+++ src/app/ofertas/page.tsx
@@ -2,6 +2,12 @@ import Layout from "@/components/layout";
 import BannerSection from "@/sections/banner-section";
 import CallToActionSection from "@/sections/call-to-action-section";
 import ProductCardSection from "@/sections/products-cards-section";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "VitrineX - Ofertas",
+  description: "Aproveite essas ofertas especiais por tempo limitado",
+};
 
 const OffersPage = async () => {
   const data = await fetch("https://fakestoreapi.com/products");
